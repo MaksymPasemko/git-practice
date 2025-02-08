c3edd839e86babad56eddf909ed1c938320247d2
1 <pasemkomax@gmail.com>
2025-02-08 10:06:19 +0200

    s

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is 4 version.");
         System.out.println("Some change in main branch.");
+        System.out.println("Change in feature3");
     }
 }
