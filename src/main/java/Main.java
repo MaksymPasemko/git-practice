df6d6db9da047d314ea0c508deea6ca2c8123209
1 <pasemkomax@gmail.com>
2025-02-08 10:38:44 +0200

    litle version is ready

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is 1 version.");
+        System.out.println("Some change in lite-version");
     }
 }
